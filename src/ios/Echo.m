10295666f46f89e1002a9cdd9ce0870201c26305
chadluangrath <cluangrath1@gmail.com>
2015-05-09 14:05:25 -0500

    Changed testing in iOS

--- src/ios/Echo.m
+++ src/ios/Echo.m
@@ -5,16 +5,13 @@
 
     - (void)echo:(CDVInvokedUrlCommand*)command
     {
-        CDVPluginResult* pluginResult = nil;
-        NSString* echo = [command.arguments objectAtIndex:0];
-
-        if (echo != nil && [echo length] > 0) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-        } else {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-        }
-
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Test Message" 
+                                                  message:@"This is a test"
+                                                 delegate:nil
+                                        cancelButtonTitle:@"OK" 
+                                        otherButtonTitles:nil];
+        [alert show];
+        [alert release];
     }
 
     @end
