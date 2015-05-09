db8f0f6b66637545ea0e56fb733da2d959695952
chadluangrath <cluangrath1@gmail.com>
2015-05-09 14:59:01 -0500

    iOS integration of the plugin works now!

--- src/ios/Echo.m
+++ src/ios/Echo.m
@@ -4,14 +4,25 @@
     @implementation Echo
 
     - (void)echo:(CDVInvokedUrlCommand*)command
-    {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Test Message" 
-                                                  message:@"This is a test"
-                                                 delegate:nil
-                                        cancelButtonTitle:@"OK" 
-                                        otherButtonTitles:nil];
-        [alert show];
-        [alert release];
+    {       
+    
+           CDVPluginResult* pluginResult = nil;
+           NSString* echo = [command.arguments objectAtIndex:0];
+    
+           UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Received Message" 
+                                                    message:echo 
+                                                    delegate:self 
+                                                    cancelButtonTitle:@"OK" 
+                                                    otherButtonTitles:nil];
+           [alert show];
+    
+           if (echo != nil && [echo length] > 0) {
+               pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+           } else {
+               pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+           }
+    
+           [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }
 
     @end
